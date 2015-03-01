07a34335a968dbb770e9e440c0b515453a67fe31
Elkin Rendón <j.erendonr@gmail.com>
2015-03-01 15:32:39 -0500

    Ahora se hace el registro completo del proyecto, es decir, se crea un proyecto con un objetivo, una meta y una actividad

--- php/registrarProyecto.php
+++ php/registrarProyecto.php
@@ -3,28 +3,44 @@ include("conexionDB.php");
 
 $conexion = mysql_connect($host,$user,$contrasena) or die ("Error al conectar con servidor: ".mysql_error());
 mysql_select_db($basedatos) or die ("Error al conectar con bases de datos: ".mysql_error());
+	//$arreglo = json_decode(stripslashes($_POST['datosFormulario']));
+//echo "Verificando...Cookie: ". $_COOKIE['codAP']. " name_ ".$_COOKIE['nameAP']." nameProy: ".$_COOKIE['nameP'];
+	//echo "Verificando...". $arreglo[0] . " nobmre: ". $arreglo[22];
+$cedula = $_COOKIE['cedulaUser'];
+$codP=$_COOKIE['codP']; $nameP=$_COOKIE['nameP']; $dateIniP=$_COOKIE['dateIniP']; $datefinP=$_COOKIE['datefinP']; 
+$montoP=$_COOKIE['montoP']; $descriP=$_COOKIE['descripcionP']; $estadoP=$_COOKIE['estadoP'];
 
-$sql = "INSERT INTO proyecto VALUES ('$_POST[codigoProy]','$_POST[nombreProy]', '$_POST[fechaIniProy]', '$_POST[fechafinProy]', '$_POST[montoProy]', '$_POST[descripcionProy]', '123456','$_POST[estadoProy]')";
-
+$sql = "INSERT INTO proyecto VALUES ('$codP','$nameP','$dateIniP','$datefinP','$montoP','$descriP','$cedula', '$estadoP')";
 $result = mysql_query($sql);
-$respuesta = new stdClass();//nuevo Ajax
 
-if(!$result){
-	//echo "Error en la sentencia SQL ".mysql_error();
-	$respuesta->mensaje ="No se pudo guardar.";//nuevo Ajax
+
+$codOP=$_COOKIE['codOP']; $nameOP=$_COOKIE['nameOP']; $dateIniOP=$_COOKIE['dateIniOP']; $datefinOP=$_COOKIE['datefinOP']; 
+$montoOP=$_COOKIE['montoOP']; $descriOP=$_COOKIE['descripOP']; $estadoOP=$_COOKIE['estadoOP'];
+
+$sqlOb = "INSERT INTO objetivo VALUES ('$codOP','$nameOP','$dateIniOP','$datefinOP','$montoOP','$descriOP','$codP','$estadoOP')";
+$result2 = mysql_query($sqlOb);
+
+
+$codMP=$_COOKIE['codMP']; $nameMP=$_COOKIE['nameMP']; $dateIniMP=$_COOKIE['dateIniMP']; $datefinMP=$_COOKIE['datefinMP']; 
+$montoMP=$_COOKIE['montoMP']; $descriMP=$_COOKIE['descripMP']; $estadoMP=$_COOKIE['estadoMP'];
+
+$sqlMeta = "INSERT INTO meta VALUES ('$codMP','$nameMP','$dateIniMP','$datefinMP','$montoMP','$descriMP','$codOP','$estadoMP')";
+$result3 = mysql_query($sqlMeta);
+
+$codAP=$_COOKIE['codAP']; $nameAP=$_COOKIE['nameAP']; $dateIniAP=$_COOKIE['dateIniAP']; $datefinAP=$_COOKIE['datefinAP']; 
+$montoAP=$_COOKIE['montoAP']; $descriAP=$_COOKIE['descriAP']; $estadoAP=$_COOKIE['estadoAP'];
+
+$sqlAct = "INSERT INTO actividad VALUES ('$codAP','$nameAP','$dateIniAP','$datefinAP','$montoAP','$descriAP','$codMP','$estadoAP')";
+$result4 = mysql_query($sqlAct);
+
+if(!$result || !$result2 || !$result3 || !$result4 ){
+	echo "Error en la sentencia SQL: ".mysql_error();
 }else{
-	//echo "Proyecto creado!";
+	echo "Proyecto creado! codAct: ".$codAP." metaID: ".$codMP." estado Acti: ".$estadoAP;
 	//echo "<br><a href='agregarObjetivo.html'> Agregar objetivo </a> ";
-	$respuesta->mensaje ="Proyecto creado.";//nuevo Ajax
 }
 @mysql_close($conexion);
 
-
-echo json_encode($respuesta);//nuevo Ajax
-
-//echo "registro";
-//echo "$_POST[fechaIniProy]";
-//echo "Proyecto creado!";
-//echo "<br><a href='../agregarObjetivo.html'> Agregar objetivo </a> ";
+//echo "Registro hecho";
 
 ?>
