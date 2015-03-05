e62ad4426544cba679df4f139183118556be39fc
Elkin Rendón <j.erendonr@gmail.com>
2015-03-05 17:35:23 -0500

    Se puede agregar un objetivo luego de haberse creado el proyecto

--- php/registrarMeta.php
+++ php/registrarMeta.php
@@ -4,21 +4,37 @@ include("conexionDB.php");
 $conexion = mysql_connect($host,$user,$contrasena) or die ("Error al conectar con servidor: ".mysql_error());
 mysql_select_db($basedatos) or die ("Error al conectar con bases de datos: ".mysql_error());
 
-$sql = "INSERT INTO meta VALUES ('$_POST[codigoMeta]','$_POST[nombreMeta]', '$_POST[fechaIniMeta]', '$_POST[fechafinMeta]', '$_POST[montoMeta]', '$_POST[descripcionMeta]', '1','$_POST[estadoMeta]')";
+//$sql = "INSERT INTO meta VALUES ('$_POST[codigoMeta]','$_POST[nombreMeta]', '$_POST[fechaIniMeta]', '$_POST[fechafinMeta]', '$_POST[montoMeta]', '$_POST[descripcionMeta]', '1','$_POST[estadoMeta]')";
 
-$result = mysql_query($sql);
-$respuesta = new stdClass();//nuevo Ajax
 
-if(!$result){
-	//echo "Error en la sentencia SQL ".mysql_error();
-	$respuesta->mensaje ="No se pudo guardar.";//nuevo Ajax
+
+$codOP=$_COOKIE['codOP'];
+$codMP=$_COOKIE['codMP']; $nameMP=$_COOKIE['nameMP']; $dateIniMP=$_COOKIE['dateIniMP']; $datefinMP=$_COOKIE['datefinMP']; 
+$montoMP=$_COOKIE['montoMP']; $descriMP=$_COOKIE['descripMP']; $estadoMP=$_COOKIE['estadoMP'];
+
+$sqlMeta = "INSERT INTO meta VALUES ('$codMP','$nameMP','$dateIniMP','$datefinMP','$montoMP','$descriMP','$codOP','$estadoMP')";
+$result3 = mysql_query($sqlMeta);
+
+
+$codAP=$_COOKIE['codAP']; $nameAP=$_COOKIE['nameAP']; $dateIniAP=$_COOKIE['dateIniAP']; $datefinAP=$_COOKIE['datefinAP']; 
+$montoAP=$_COOKIE['montoAP']; $descriAP=$_COOKIE['descriAP']; $estadoAP=$_COOKIE['estadoAP'];
+
+$sqlAct = "INSERT INTO actividad VALUES ('$codAP','$nameAP','$dateIniAP','$datefinAP','$montoAP','$descriAP','$codMP','$estadoAP')";
+$result4 = mysql_query($sqlAct);
+
+
+
+//$result = mysql_query($sql);
+///$respuesta = new stdClass();//nuevo Ajax
+
+if(!$result3 || !$result4){
+	echo "No se puedo agregar la meta: ".mysql_error();
+	
 }else{
-	//echo "Meta agregada!";
-	//echo "<br><a href='../agregarObjetivo.html'> Agregar objetivo </a> ";
-	//echo "<br><a href='../agregarMeta.html'> Agregar meta </a> ";
-	//echo "<br><a href='../agregarActividad.html'> Agregar actividad </a> ";
-	$respuesta->mensaje ="Proyecto creado.";//nuevo Ajax
+	echo "Meta agregada!";
+	
+	//$respuesta->mensaje ="Meta agregada!";//nuevo Ajax
 }
 @mysql_close($conexion);
-echo json_encode($respuesta);//nuevo Ajax
+//echo json_encode($respuesta);//nuevo Ajax
 ?>
--- php/registrarObjetivo.php
+++ php/registrarObjetivo.php
@@ -4,21 +4,44 @@ include("conexionDB.php");
 $conexion = mysql_connect($host,$user,$contrasena) or die ("Error al conectar con servidor: ".mysql_error());
 mysql_select_db($basedatos) or die ("Error al conectar con bases de datos: ".mysql_error());
 
-$sql = "INSERT INTO objetivo VALUES ('$_POST[codigoObj]','$_POST[nombreObj]', '$_POST[fechaIniObj]', '$_POST[fechafinObj]', '$_POST[montoObj]', '$_POST[descripcionObj]',    'PRDIMA01'    ,'$_POST[estadoObj]')";
+//$sql = "INSERT INTO objetivo VALUES ('$_POST[codigoObj]','$_POST[nombreObj]', '$_POST[fechaIniObj]', '$_POST[fechafinObj]', '$_POST[montoObj]', '$_POST[descripcionObj]',    'PRDIMA01'    ,'$_POST[estadoObj]')";
 
-$result = mysql_query($sql);
-$respuesta = new stdClass();
+$codP=$_COOKIE['codP'];
+$codOP=$_COOKIE['codOP']; $nameOP=$_COOKIE['nameOP']; $dateIniOP=$_COOKIE['dateIniOP']; $datefinOP=$_COOKIE['datefinOP']; 
+$montoOP=$_COOKIE['montoOP']; $descriOP=$_COOKIE['descripOP']; $estadoOP=$_COOKIE['estadoOP'];
 
-if(!$result){
-	//echo "Error en la sentencia SQL ".mysql_error();
-	$respuesta->mensaje ="Error en la sentencia";
+$sqlOb = "INSERT INTO objetivo VALUES ('$codOP','$nameOP','$dateIniOP','$datefinOP','$montoOP','$descriOP','$codP','$estadoOP')";
+$result2 = mysql_query($sqlOb);
+
+
+$codMP=$_COOKIE['codMP']; $nameMP=$_COOKIE['nameMP']; $dateIniMP=$_COOKIE['dateIniMP']; $datefinMP=$_COOKIE['datefinMP']; 
+$montoMP=$_COOKIE['montoMP']; $descriMP=$_COOKIE['descripMP']; $estadoMP=$_COOKIE['estadoMP'];
+
+$sqlMeta = "INSERT INTO meta VALUES ('$codMP','$nameMP','$dateIniMP','$datefinMP','$montoMP','$descriMP','$codOP','$estadoMP')";
+$result3 = mysql_query($sqlMeta);
+
+
+$codAP=$_COOKIE['codAP']; $nameAP=$_COOKIE['nameAP']; $dateIniAP=$_COOKIE['dateIniAP']; $datefinAP=$_COOKIE['datefinAP']; 
+$montoAP=$_COOKIE['montoAP']; $descriAP=$_COOKIE['descriAP']; $estadoAP=$_COOKIE['estadoAP'];
+
+$sqlAct = "INSERT INTO actividad VALUES ('$codAP','$nameAP','$dateIniAP','$datefinAP','$montoAP','$descriAP','$codMP','$estadoAP')";
+$result4 = mysql_query($sqlAct);
+
+
+
+//$result = mysql_query($sql);
+//$respuesta = new stdClass();
+
+if(!$result2 || !$result3 || !$result4 ){
+	echo "No se pudo agregar el objetivo: ".mysql_error();
+	//$respuesta->mensaje ="Error en la sentencia";
 }else{
-	//echo "Objetivo agregado!";
+	echo "Objetivo agregado.";
 	//echo "<br><a href='../agregarObjetivo.html'> Agregar objetivo </a> ";
 	//echo "<br><a href='../agregarMeta.html'> Agregar meta </a> ";
-	$respuesta->mensaje ="Objetivo agregado";
+	//$respuesta->mensaje ="Objetivo agregado";
 }
 @mysql_close($conexion);
-echo json_encode($respuesta);
+//echo json_encode($respuesta);
 
 ?>
