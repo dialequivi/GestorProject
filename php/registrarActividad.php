90378245ff2da7eb2b3cc81479ddf0932ca61682
Elkin Rendón <j.erendonr@gmail.com>
2015-03-06 09:59:50 -0500

    Registra una nueva actividad que se quiere agregar. PD: Se agrega a la ultima meta del priyecto

--- php/registrarActividad.php
+++ php/registrarActividad.php
@@ -1,32 +1,33 @@
 <?php 
 	//FALTA ORGANIZAR LO DEL ARCHIVO ADJUNTO PARA PODER EJECUTAR LA SENTENCIA Y AGREGUE A LA BD
-	include("conexionDB.php");
-	$conexion = mysql_connect($host,$user,$contrasena) or die ("Error al conectar con servidor: ".mysql_error());
-	mysql_select_db($basedatos) or die ("Error al conectar con bases de datos: ".mysql_error());
+include("conexionDB.php");
+$conexion = mysql_connect($host,$user,$contrasena) or die ("Error al conectar con servidor: ".mysql_error());
+mysql_select_db($basedatos) or die ("Error al conectar con bases de datos: ".mysql_error());
 
 
 	//Verificar tipo de archivo adjuntado
 	//$adjuntosPermitidos = array("image/jpg", "imagle/jpeg", "image/gif", "image/png");
+$codMP = $_COOKIE['codMP'];
+$codAP=$_COOKIE['codAP']; $nameAP=$_COOKIE['nameAP']; $dateIniAP=$_COOKIE['dateIniAP']; $datefinAP=$_COOKIE['datefinAP']; 
+$montoAP=$_COOKIE['montoAP']; $descriAP=$_COOKIE['descriAP']; $estadoAP=$_COOKIE['estadoAP'];
+
+$sqlAct = "INSERT INTO actividad VALUES ('$codAP','$nameAP','$dateIniAP','$datefinAP','$montoAP','$descriAP','$codMP','$estadoAP')";
+$result = mysql_query($sqlAct);
 
 
-	$sql = "INSERT INTO actividad VALUES ('$_POST[codigoActi]', '$_POST[nombreActi]', '$_POST[fechaIniActi]', '$_POST[fechafinActi]',
-		'$_POST[montoActi]', '$_POST[descripcionActi]', 
-		'$ADJUNTO',
-		'1.1',
-		'$_POST[estadoActi]' )";
+/*	'$_POST[montoActi]', '$_POST[descripcionActi]', 
+	'$ADJUNTO',
+	'1.1',
+	'$_POST[estadoActi]' )";*/
 
-	$result = mysql_query($sql);
-	$respuesta = new stdClass();//nuevo Ajax
-
-	if(!$result){
-		//echo "Error en la sentencia SQL ".mysql_error();
-		$respuesta->mensaje ="No se pudo guardar.";//nuevo Ajax
-	}else{
-		//echo "Proyecto creado!";
-		//echo "<br><a href='agregarObjetivo.html'> Agregar objetivo </a> ";
-		$respuesta->mensaje ="Proyecto creado.";//nuevo Ajax
-	}
-	@mysql_close($conexion);
-	echo json_encode($respuesta);//nuevo Ajax
+if(!$result){
+	echo "No se puedo agregar la actividad: ".mysql_error();
+	
+}else{
+	echo "Actividad agregada!";
+	
+	//$respuesta->mensaje ="Meta agregada!";//nuevo Ajax
+}
+@mysql_close($conexion);
 
 ?>
